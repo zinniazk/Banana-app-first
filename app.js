01376ea043ee0f8c925ae83f76a2563e9c046313
zinniazk <zinniak_91@hotmail.com>
2025-03-01 09:46:19 +0000

    changes to URL of API

--- app.js
+++ app.js
@@ -2,11 +2,25 @@ var txtinput = document.querySelector("#inputtxt");
 var buttonclick = document.querySelector("#btn-translate");
 var outputtxt   = document.querySelector("#output");
 
-console.log(txtinput.value);
+// console.log(txtinput.value);
+
+var serverURL  = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(input){
+    return serverURL+ "?" + "text" +input;
+}
+
+
 // outputtxt.innerHTML = "Zinnia Katib";
 function minionClick() {
-    console.log("clicked");
-    console.log("you have typed" + txtinput.value);
-    outputtxt.innerText = "Minion Language" + txtinput.value;
-};
+    inputtxt = txtinput.value;
+
+    fetch(getTranslationURL(inputtxt))
+        .then(response => response.json())
+        .then(json => {
+            var translatedtext = json.contents.translated;
+            outputDiv.innerText = translatedtext;
+        })
+    // outputtxt.innerText = "Minion Language  " + txtinput.value;
+    };
 buttonclick.addEventListener("click", minionClick);
